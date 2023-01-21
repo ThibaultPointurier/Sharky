2b7d5a519427d382de522fce7bf62a66d8cef583
Azarow <contactazarox@gmail.com>
2023-01-22 00:47:34 +0100

    delete ready events

--- src/main.dart
+++ src/main.dart
@@ -2,13 +2,12 @@ import 'package:mineral/core.dart';
 
 import 'commands/setup.dart';
 import 'events/autorole_interaction.dart';
-import 'events/ready.dart';
 
 void main () async {
   Kernel kernel = Kernel()
     ..intents.defined(all: true)
     ..commands.register([new Setup()])
-    ..events.register([new AutoroleInteraction(), new Ready()])
+    ..events.register([new AutoroleInteraction()])
     ..modules.register([])
     ..contextMenus.register([]);
 
