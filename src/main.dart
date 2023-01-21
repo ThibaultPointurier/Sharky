00dbb631711756228a92be5e03b0c34b64dc3af3
Azarow <contactazarox@gmail.com>
2023-01-22 00:41:11 +0100

    register events

--- src/main.dart
+++ src/main.dart
@@ -2,12 +2,13 @@ import 'package:mineral/core.dart';
 
 import 'commands/setup.dart';
 import 'events/autorole_interaction.dart';
+import 'events/ready.dart';
 
 void main () async {
   Kernel kernel = Kernel()
     ..intents.defined(all: true)
     ..commands.register([new Setup()])
-    ..events.register([new AutoroleInteraction()])
+    ..events.register([new AutoroleInteraction(), new Ready()])
     ..modules.register([])
     ..contextMenus.register([]);
 
